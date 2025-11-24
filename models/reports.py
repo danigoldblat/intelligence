7c4cf168d008b8e3edc6f6e9e76d5738788e57da
danigoldblat <d0504109212@gmail.com>
2025-11-24 15:47:48 +0200

    schama of reports

---
+++ models/reports.py
@@ -0,0 +1,14 @@
+from sqlmodel import SQLModel, Field
+from sqlmodel import create_engine
+from sqlmodel import Session, select
+from agents import Agents
+import time
+
+class Reports(SQLModel,table=True):
+    Id:int=Field(default=None,primary_key=True)
+    AgentsId:int=Field(default=None,foreign_key="Agents.id")
+    TerroristsId:int=Field(default=None,foreign_key="terrorists.id")
+    date:str
+    category:str
+    detail:str
+engine=create_engine("mysql+mysqlconnector://root:@localhost/intelligence?use_pure=True") 
